b2b779a77e78dc75bf166aed374d26765500ec76
Adele Medina <adelemdina@gmail.com>
2025-06-07 12:40:40 -0400

    estilos nav

--- projects/hola-mundo/src/componets/Header.jsx
+++ projects/hola-mundo/src/componets/Header.jsx
@@ -3,9 +3,21 @@ import { NavBar } from './NavBar';
 export const Header = () => {
     return (
      <header class="flex place-content-between">
-        <div class="logo">
+      <div class="logo w-[90.53px] h-[70px] flex items-center bg-amber-300">
            <img src={logo} alt="" />
         </div>
+        <div class="flex justify-end items-center min-h-[80px]  px-4">
+      <div class="flex  rounded-full overflow-hidden border border-gray-200">
+        <button class="flex items-center gap-2 px-4 py-1 bg-blue-700 text-white font-semibold focus:outline-none">
+          <span class="text-xl">🇪🇸</span>
+          <span>Español</span>
+        </button>
+        <button class="flex items-center gap-2 px-4 py-1 text-gray-700 font-semibold focus:outline-none">
+          <span class="text-xl">🇺🇸</span>
+          <span>Ingles</span>
+        </button>
+      </div>
+    </div>
       <NavBar />
     </header>
     )
--- projects/hola-mundo/src/componets/NavBar.jsx
+++ projects/hola-mundo/src/componets/NavBar.jsx
@@ -3,7 +3,7 @@
 export const NavBar = () => {
   return (
   <ul class="bg-amber-100text-white hidden flex sm:flex
-  text-[16px] font-regular w[438px]
+  text-[16px] font-regular w[438px] bg-amber-200 w-full flex gap-6 justify-center
   ">
     
    <li>
@@ -19,7 +19,14 @@ export const NavBar = () => {
    <li>
     Trabajos Realizados
    </li>
-    <li><a href="#contacto" class="contact-btn">Contacto</a></li>
+    <li><button className="bg-white text-blue-600 font-bold py-2 px-4 rounded-full cursor-pointer flex items-center gap-2 hover:bg-blue-600 hover:text-white transition-colors duration-300">
+          <span>Contáctame</span>
+          <span className="w-6 h-6 flex items-center justify-center rounded-full bg-blue-600 text-white">
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 17l10-10" />
+            </svg>
+          </span>
+        </button></li>
   </ul>
   
    
